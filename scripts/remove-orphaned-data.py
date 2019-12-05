df83966e95d246619b05168be768ce3a75e992f4
Carl <chris.carl@intel.com>
2019-12-05 03:16:26 -0800

    added a remove bad data script

--- scripts/remove-orphaned-data.py
+++ scripts/remove-orphaned-data.py
@@ -4,7 +4,9 @@ import datetime
 import logging
 import argparse
 
+import mongoengine
 from mongoengine import connect
+from bson import ObjectId
 from apilib.lib import augment_args
 from database.models.project import Project
 from database.models.device import Device
@@ -16,7 +18,6 @@ LOGGER.addHandler(logging.NullHandler())
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     execution = parser.add_argument_group()
-    execution.add_argument('-p', '--project', type=str, help='Project name as a string.')
     execution.add_argument(
         '-e', '--env', action='store_true', default=os.path.abspath('env.xml'), help='Env XML File'
     )
@@ -41,19 +42,15 @@ if __name__ == "__main__":
     )
 
     # https://stackoverflow.com/a/18266321
-    projects_to_delete = [proj for proj in Project.objects(name=args.project)]
-    devices_to_delete = [dev for dev in Device.objects(meta_data__project=args.project)]
-    data_to_delete = [data for dev in devices_to_delete for data in RawData.objects(device=dev.id)]
+    data_to_delete = []
+    for data in RawData.objects():
+        try:
+            data.device
+        except mongoengine.errors.DoesNotExist:
+            data_to_delete.append(data)
 
-    damned_projects = [p.id for p in projects_to_delete]
-    damned_devices = [d.id for d in devices_to_delete]
-    damned_data = [d.id for d in data_to_delete]
 
-    print(
-        'removing {} project: \n{}'.format(len(damned_projects), [str(e) for e in damned_projects])
-    )
-    print('removing {} devices: \n{}'.format(len(damned_devices), [str(e) for e in damned_devices]))
-    print('removing {} data: \n{}'.format(len(damned_data), [str(e) for e in damned_data]))
+    print('removing {}'.format(len(data_to_delete)))
 
     if not args.yes:
         if sys.version_info[0] == 2:
@@ -64,10 +61,6 @@ if __name__ == "__main__":
             args.yes = str(inp).lower()[0] == 'y'
 
     if args.yes:
-        for p in projects_to_delete:
-            p.delete()
-        for d in devices_to_delete:
-            d.delete()
         for d in data_to_delete:
             d.delete()
     else:
